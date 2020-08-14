81bdf8b4bbdf0e191fae27d4756a3ed52efa4e52
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-08-14 10:21:55 -0600

    step up complete

--- helo/server/index.js
+++ helo/server/index.js
@@ -1,11 +1,29 @@
-// require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 // const session = require('express-session');
-// const massive = require('massive');
+const massive = require('massive');
 const app = express();
 const ctrl = require('./controller');
 
+const {CONNECTION_STRING, SESSION_SECRET, SERVER_PORT} = process.env;
 
+
+// app.use(session({
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { maxAge: 1000 * 60 * 60 * 48},
+//     secret: SESSION_SECRET
+// }))
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+    }).then( db => {
+        app.set('db', db)
+        console.log('connected to db')
+    }).catch( err => console.log(err))
 
 app.use(express.json());
 
